9bce9aec38cbc2a349b0e86e4729e8cafca352e8
Mario Ricardo Rosas Benitez <Mariorosas7711@gmail.com>
2025-11-07 16:25:13 -0600

    Sprint4: Ruta Auth corregida

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { login } from "../api/auth"; // ✅ tu API original
+import { login } from "../Api/auth"; // ✅ tu API original
 import styles from "../styles/Login.module.css";
 import { useAuth } from "../hook/useAuth"; // ✅ usamos el contexto
 
